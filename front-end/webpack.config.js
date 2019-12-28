6870356432869e0c573c442f2628cc5aa5297850
tandavala <jose.tandavala@gmail.com>
2019-12-28 15:59:25 +0100

    programando a primeira configuracao do webpack

---
+++ front-end/webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
