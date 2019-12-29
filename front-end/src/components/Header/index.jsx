611613f81548ae11241e9a512682c5c7c7b8fdd6
tandavala <jose.tandavala@gmail.com>
2019-12-29 09:11:20 +0100

    programando o cabecalho

---
+++ front-end/src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header pure-menu pure-menu-horizontal pure-menu-fixed">
+                <a href="/">
+                    <img src="/img/logo.png" className="logo" alt="logo" />
+                    <h4 className="label">Agenda de Gentilezas</h4>
+                </a>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './img/favicon.ico'
+import './img/logo.png'
 import './css/index.css'
 import './css/pure-min.css'
 
+import App from './components/App'
+
 ReactDOM.render(
-    <h1>Bem-vindo ao React!...</h1>,
+    <App />,
     document.querySelector("#main")
 )
