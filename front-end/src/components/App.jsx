463ddb9aa75c93fd5de477bd5809caffc4db9568
tandavala <jose.tandavala@gmail.com>
2019-12-29 09:35:47 +0100

    programando o component NovoUsuario e fazendo o primeiro deploy

--- front-end/src/components/App.jsx
+++ front-end/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Header from './Header'
+import NovoUsuario from './NovoUsuario'
 
 class App extends Component {
     render() {
         return (
-            <Header />
+            <div>
+                <Header />
+                <NovoUsuario />
+            </div>
         )
     }
 }
---
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class NovoUsuario extends React.Component {
+    render() {
+        return (
+            <div className="center">
+                <h3>Novo usuario</h3>
+            </div>
+        )
+    }
+}
+
+export default NovoUsuario
