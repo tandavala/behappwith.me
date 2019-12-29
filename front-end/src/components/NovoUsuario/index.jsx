0a86204e4762cb350c2e68e8c9e3e583522bcf24
tandavala <jose.tandavala@gmail.com>
2019-12-29 11:01:27 +0100

    refactorando o component NovoUsuario

--- front-end/src/components/NovoUsuario/index.jsx
+++ front-end/src/components/NovoUsuario/index.jsx
@@ -4,13 +4,40 @@ import Label from '../Label'
 import Input from '../Input'
 
 class NovoUsuario extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            usuario: {
+                nome: ''
+            },
+            validacao: {
+                nomeInvalido: true
+            }
+
+        }
+    }
+    atualizarNome(e) {
+        let usuario = this.state.usuario;
+        usuario.nome = e.target.value;
+        this.setState({
+            usuario: usuario
+        });
+    }
     render() {
 
         return (
             <div className="center">
                 <form className="pure-form pure-form-stacked">
-                    <Label htmlFor="nome" texto="Quem e voce?" />
-                    <Input id="nome" placeholder="Informar o nome" valorInvalido />
+                    <Label htmlFor="nome" texto="Quem e voce?" valorInvalido={this.state.validacao.nomeInvalido} />
+                    <Input
+                        id="nome"
+                        name="nome"
+                        readOnly={false}
+                        placeholder="Digite o teu nome"
+                        maxLength="40" defaultValue="tandavala"
+                        valorInvalido={this.state.validacao.nomeInvalido}
+                        onChange={this.atualizarNome.bind(this)}
+                    />
                 </form>
             </div>
         )
