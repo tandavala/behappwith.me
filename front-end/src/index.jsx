53906046db28bfbdb499e0b229e4e41452247d08
tandavala <jose.tandavala@gmail.com>
2019-12-30 05:04:17 +0100

    programando o component GenderImage

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './img/favicon.ico'
 import './img/logo.png'
+import './img/avatars.png'
 import './css/index.css'
 import './css/pure-min.css'
 
