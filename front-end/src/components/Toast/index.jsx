fe8dc6acf7c3061afea5bb12294c9616ddafe1f1
tandavala <jose.tandavala@gmail.com>
2019-12-30 08:11:15 +0100

    programando o compente Toast

---
+++ front-end/src/components/Toast/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.min.css'
+
+class Toast extends React.Component {
+    successo(msg) {
+        toast.success(msg)
+    }
+    info(msg) {
+        toast.info(msg)
+    }
+    error(msg) {
+        toast.error(msg)
+    }
+
+    render() {
+        return (
+            <ToastContainer
+                position="bottom-center"
+                autoClose={5000}
+                hideProgressBar="true"
+                closeOnClick
+                pauseOnHover
+            />
+        )
+    }
+}
+export default Toast
