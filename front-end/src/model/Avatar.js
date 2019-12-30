b6a60be12ee472888f952ae7ef000bcb86d47ba4
tandavala <jose.tandavala@gmail.com>
2019-12-30 10:30:34 +0100

    programando a classe avatar

---
+++ front-end/src/model/Avatar.js
@@ -0,0 +1,16 @@
+class Avatar {
+    constructor(index, descricao) {
+        this.index = index
+        this.descricao = descricao
+    }
+    toString() {
+        return this.descricao;
+    }
+    static obterTodos() {
+        return Array(23).fill(0).map((entrey, i) => {
+            return new Avatar(i, `Avatar ${i + 1}`)
+        })
+    }
+}
+
+export default Avatar
