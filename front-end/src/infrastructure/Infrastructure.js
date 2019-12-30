c0377cce5443ec22b7ceafd1e0c63524d9eac35f
tandavala <jose.tandavala@gmail.com>
2019-12-30 14:47:37 +0100

    programando a infrasturura que sera responsavel pela persistencia da base de dados.

---
+++ front-end/src/infrastructure/Infrastructure.js
@@ -0,0 +1,21 @@
+class Repostory {
+    constructor() {
+        this.key = 'behappy-user'
+    }
+    salvar(json, callback) {
+        let data = JSON.stringify(json)
+        window.localStorage.setItem(this.key, data)
+        callback()
+    }
+    obter(successo, falha) {
+        let data = windows.localStorage.getItem(this.key)
+        let json = JSON.parse(data)
+
+        if (json) {
+            successo(json)
+        } else {
+            falha()
+        }
+    }
+}
+export default Repostory
