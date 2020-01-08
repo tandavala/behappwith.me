2292561f858ea61b836bf3282499835c059cdf63
tandavala <jose.tandavala@gmail.com>
2020-01-08 20:58:56 +0100

    programando o component fixedbutton

---
+++ front-end/src/components/FixedButton/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "../Image";
+import "./index.css";
+import { withRouter } from "react-router-dom";
+
+const FixedButton = withRouter(props => {
+  let type = props.type ? props.type : "primary";
+  const className = `fixed-button ${type} pure-button`;
+
+  return (
+    <button
+      className={className}
+      onClick={e => {
+        e.preventDefault();
+
+        if (props.url && props.onClick) {
+          props.onClick(() => {
+            props.history.push(props.url);
+          });
+        } else if (props.url) {
+          props.history.push(props.url);
+        } else {
+          props.onClick();
+        }
+      }}
+    >
+      <Image
+        eixoX={props.index}
+        eixoY={0}
+        width={48}
+        height={48}
+        backgroundHeight={48}
+        arquivo="img/botoes.png"
+      />
+    </button>
+  );
+});
+
+export default FixedButton;
