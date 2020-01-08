9284182e23794d400462a2732108e5239dde7c7c
tandavala <jose.tandavala@gmail.com>
2020-01-08 18:28:12 +0100

    trabalhando no serveworker

--- front-end/dist/behappy.sw.js
+++ front-end/dist/behappy.sw.js
@@ -3,21 +3,66 @@ var versao = 1;
 var idAtual = identificador + "-" + versao;
 var idAnterior = identificador + "-" + (versao - 1);
 
-var ulrs = [
+var urls = [
   "/",
   "bundle.js",
   "style.css",
   "img/avatars.png",
   "img/botoes.png",
-  "img/logo.png"
+  "img/favicon.ico",
+  "img/logo.png",
+  "img/icon-72x72.png",
+  "img/icon-96x96.png",
+  "img/icon-128x128.png",
+  "img/icon-144x144.png",
+  "img/icon-152x152.png",
+  "img/icon-192x192.png",
+  "img/icon-384x384.png",
+  "img/icon-512x512.png"
 ];
 
-function ativarServiceWOrker() {
+function instalarServiceWorker() {
+  console.log("ServiceWorker instalado com sucesso!");
+}
+
+function ativarServiceWorker() {
   caches.open(idAtual).then(cache => {
     console.log("Cache Storage " + idAtual + " foi ativado com sucesso!");
+
     cache.addAll(urls).then(function() {
       caches.delete(idAnterior);
-      console.log("Cache Storage " + idAnterior + " foi axcluido");
+      console.log("Cache Storage " + idAnterior + " foi excluído!");
     });
   });
 }
+
+function buscarArquivos(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(arquivoCache) {
+      return arquivoCache ? arquivoCache : fetch(event.request);
+    })
+  );
+}
+
+function buscarArquivosComSalvamento(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(arquivoCache) {
+      if (arquivoCache) {
+        return arquivoCache;
+      }
+
+      var cloneDoRequest = event.request.clone();
+      return fetch(cloneDoRequest).then(function(response) {
+        var cloneDoResponse = response.clone();
+        caches.open(idAtual).then(function(cache) {
+          cache.put(event.request, cloneDoResponse);
+        });
+        return response;
+      });
+    })
+  );
+}
+
+self.addEventListener("install", instalarServiceWorker);
+self.addEventListener("activate", ativarServiceWorker);
+self.addEventListener("fetch", buscarArquivos);
--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,49 +1,54 @@
-const webpack = require('webpack')
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglyfyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglyfyJSPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.jsx',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+  entry: "./src/index.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(jpeg|ico|png|gif|svg)$/i,
+        loader: "file-loader?name=img/[name].[ext]"
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
 
-            },
-            {
-                test: /\.(jpeg|ico|png|gif|svg)$/i,
-                loader: 'file-loader?name=img/[name].[ext]'
-            }, {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
-            }
-        ]
-    },
-
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: path.join(__dirname, 'src/index.html')
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.css'
-        }),
-        new UglyfyJSPlugin()
-    ],
-    devServer: {
-        publicPath: '/',
-        contentBase: './dist'
-    }
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src/index.html")
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new UglyfyJSPlugin()
+  ],
+  devServer: {
+    publicPath: "/",
+    contentBase: "./dist"
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: "./dist",
+    headers: { "Cache-Control": "max-age=600" }
+  }
 };
