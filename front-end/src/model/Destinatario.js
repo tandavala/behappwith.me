87bc127703d91a5fec2963ae29ba9b6790aff477
tandavala <jose.tandavala@gmail.com>
2020-01-08 20:48:48 +0100

    programando o modelo destinatario

---
+++ front-end/src/model/Destinatario.js
@@ -0,0 +1,36 @@
+class Destinatario {
+  constructor(index, descricao, frase) {
+    this.index = index;
+    this.descricao = descricao;
+  }
+  toString() {
+    return this.descricao;
+  }
+  static parse(json) {
+    return new Destinatario(json.index, json.descricao);
+  }
+  static obterTodos() {
+    let destinatarios = [
+      new Destinatario(0, "Pais"),
+      new Destinatario(1, "Filhos"),
+      new Destinatario(2, "Cônjuges"),
+      new Destinatario(3, "Amigos"),
+      new Destinatario(4, "Pets"),
+      new Destinatario(5, "Outros")
+    ];
+    return destinatarios;
+  }
+  static obterTodasPessoas() {
+    let todos = Destinatario.obterTodos();
+    let pessoas = [
+      todos[0], // Pais
+      todos[1], // Filhos
+      todos[2], // Cônjuges
+      todos[3], // Amigos
+      todos[5] // Outros
+    ];
+    return pessoas;
+  }
+}
+
+export default Destinatario;
